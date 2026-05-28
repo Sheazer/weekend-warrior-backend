36899483c27beff86fd9c3ea59c7ec5e3f391fb4
Tashmatov Bahtier <u.n_re_a.l@kstu.kg>
2026-05-28 21:07:55 +0600

    добавлены координаты для активности

--- internal/models/activity.go
+++ internal/models/activity.go
@@ -4,21 +4,27 @@ import "gorm.io/gorm"
 
 type Activity struct {
 	gorm.Model
-	Title          string        `json:"title"`
-	Description    string        `json:"description"`
-	Category       string        `json:"category"`                     // Например, "sports"
-	Date           string        `json:"date"`                         // Для простоты пока строка, потом перейдем на time.Time
-	Status         string        `json:"status" gorm:"default:active"` // active, cancelled, finished
-	MaxPeople      int           `json:"max_people"`
-	OrganizerID    uint          `json:"organizer_id"`
-	NeedModeration bool          `json:"need_moderation"` // true = ручное подтверждение
-	Organizer      User          `json:"organizer,omitempty" gorm:"foreignKey:OrganizerID"`
-	Participants   []Participant `json:"participants,omitempty"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Category    string `json:"category"`
+	Date        string `json:"date"`
+	Status      string `json:"status" gorm:"default:active"`
+	MaxPeople   int    `json:"max_people"`
+
+	// координаты
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+
+	OrganizerID    uint `json:"organizer_id"`
+	NeedModeration bool `json:"need_moderation"`
+
+	Organizer    User          `json:"organizer,omitempty" gorm:"foreignKey:OrganizerID"`
+	Participants []Participant `json:"participants,omitempty"`
 }
 
 type Message struct {
 	gorm.Model
-	SenderID	   int    `json:"sender_id"` 
-	ActivityID     int    `json:"activity_id"`
-	Content        string `json:"content"`
+	SenderID   int    `json:"sender_id"`
+	ActivityID int    `json:"activity_id"`
+	Content    string `json:"content"`
 }
